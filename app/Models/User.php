686ee2a5178b6c30dbef4dc2cf2cdbc6c30de97b
Josh Campbell <josh.campbell4000@gmail.com>
2023-07-31 23:20:50 +0930

    update user model with required table relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,6 +43,19 @@ class User extends Authenticatable
     public const FIELD_WEBSITE = 'website';
     public const FIELD_ADDRESS = 'address';
 
+    public const RELATION_REFEREES = 'referees';
+    public const RELATION_EDUCATION = 'education';
+    public const RELATION_WORK_EXPERIENCES = 'workExperiences';
+    public const RELATION_QUALIFICATIONS = 'qualifications';
+    public const RELATION_PUBLICATIONS = 'publications';
+    public const RELATION_LICENCES = 'licences';
+    public const RELATION_CERTIFICATIONS = 'certifications';
+    public const RELATION_ENTITY_CONTACT = 'entityContact';
+    public const RELATION_COVER_LETTERS = 'coverLetters';
+    public const RELATION_RESUMES = 'resumes';
+    public const RELATION_JOB_APPLICATIONS = 'jobApplications';
+
+
     /**
      * The attributes that are mass assignable.
      *
@@ -72,4 +86,84 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * @return HasMany 
+     */
+    public function referees(): HasMany
+    {
+        return $this->hasMany(UserReferee::class, UserReferee::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function workExperiences(): HasMany
+    {
+        return $this->hasMany(UserWorkExperience::class, UserWorkExperience::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function qualifications(): HasMany
+    {
+        return $this->hasMany(UserQualification::class, UserQualification::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function publications(): HasMany
+    {
+        return $this->hasMany(UserPublication::class, UserPublication::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function licences(): HasMany
+    {
+        return $this->hasMany(UserLicence::class, UserLicence::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function certifications(): HasMany
+    {
+        return $this->hasMany(UserCertification::class, UserCertification::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function entityContact(): HasMany
+    {
+        return $this->hasMany(EntityContact::class, EntityContact::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function coverLetters(): HasMany
+    {
+        return $this->hasMany(UserCoverLetter::class, UserCoverLetter::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function resumes(): HasMany
+    {
+        return $this->hasMany(UserResume::class, UserResume::FIELD_USER_ID, self::FIELD_ID);
+    }
+
+    /**
+     * @return HasMany 
+     */
+    public function jobApplications(): HasMany
+    {
+        return $this->hasMany(UserJobApplication::class, UserResume::FIELD_USER_ID, self::FIELD_ID);
+    }
 }
