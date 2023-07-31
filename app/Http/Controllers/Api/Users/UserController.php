d979bc6bdbd63f61a75ecfebb5690fc6bf182ac9
Josh Campbell <josh.campbell4000@gmail.com>
2023-07-31 23:21:14 +0930

    create API actions for getting user profile and updating bio

--- app/Http/Controllers/Api/Users/UserController.php
+++ app/Http/Controllers/Api/Users/UserController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api\Users;
 
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Requests\User\Profile\UserProfileRequest;
+use App\Http\Resources\User\Profile\UserProfileResource;
 use App\Models\User;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends ApiController
 {
@@ -17,14 +20,47 @@ class UserController extends ApiController
     }
 
     /**
-     * @param Request $request 
+     * @param UserProfileRequest $request 
+     * @return JsonResponse 
+     */
+    public function getProfile(UserProfileRequest $request): JsonResponse
+    {
+        $data = $request->collect();
+
+        return $this->formatResponse([
+            UserProfileRequest::REQUEST_USER_PROFILE => new UserProfileResource(Auth::user())
+        ]);
+    }
+
+    /**
+     * @param UserProfileRequest $request 
      * @return JsonResponse 
      * @throws BindingResolutionException 
      */
-    public function getReferees(Request $request): JsonResponse
+    public function getReferees(UserProfileRequest $request): JsonResponse
     {
+        $data = $request->collect();
+
         return $this->formatResponse([
-            'referees' => User::all()
+            UserProfileRequest::REQUEST_REFEREES => Auth::user()->{User::RELATION_REFEREES}
         ]);
     }
+
+    /**
+     * @param UserProfileRequest $request 
+     * @return JsonResponse 
+     * @throws BindingResolutionException 
+     */
+    public function updateBio(UserProfileRequest $request): JsonResponse
+    {
+        $data = $request->collect();
+        /** @var User $user */
+        $user = Auth::user();
+        $user->{User::FIELD_BIO} = $data[UserProfileRequest::REQUEST_BIO];
+
+        return $this->formatResponse([
+            'status' => $user->save()
+        ]);
+    }
+
 }
