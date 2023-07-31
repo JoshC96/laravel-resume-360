2977a9177f90476225ddbc8c5c118bccf43d82f3
Josh Campbell <josh.campbell4000@gmail.com>
2023-07-31 23:20:27 +0930

    create UserProfileRequest and UserProfileResource classes

---
+++ app/Http/Requests/User/Profile/UserProfileRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\User\Profile;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserProfileRequest extends FormRequest
+{
+
+    public const REQUEST_USER_PROFILE = 'profile';
+    public const REQUEST_BIO = 'bio';
+    public const REQUEST_REFEREES = 'referees';
+    public const REQUEST_WORK_HISTORY = 'workHistory';
+    public const REQUEST_QUALFICATIONS = 'qualifications';
+    public const REQUEST_CERTIFICATIONS = 'certifications';
+    public const REQUEST_LICENSES = 'licenses';
+    public const REQUEST_PUBLICATIONS = 'publications';
+
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::user() ? true : false;
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+        ];
+    }
+}
---
+++ app/Http/Resources/User/Profile/UserProfileResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\User\Profile;
+
+use App\Http\Requests\User\Profile\UserProfileRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $user = $this->resource;
+
+        return [
+            UserProfileRequest::REQUEST_BIO => $user->{User::FIELD_BIO} ?? '',
+            UserProfileRequest::REQUEST_REFEREES => $user->{User::RELATION_REFEREES},
+            UserProfileRequest::REQUEST_QUALFICATIONS => $user->{User::RELATION_QUALIFICATIONS},
+            UserProfileRequest::REQUEST_WORK_HISTORY => $user->{User::RELATION_WORK_EXPERIENCES},
+            UserProfileRequest::REQUEST_CERTIFICATIONS => $user->{User::RELATION_CERTIFICATIONS},
+            UserProfileRequest::REQUEST_LICENSES => $user->{User::RELATION_LICENCES},
+            UserProfileRequest::REQUEST_PUBLICATIONS => $user->{User::RELATION_PUBLICATIONS},
+        ];
+    }
+}
