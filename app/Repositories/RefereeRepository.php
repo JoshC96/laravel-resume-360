5760131f62e4068a02090509b26019b5f4352fc3
Josh Campbell <josh.campbell4000@gmail.com>
2023-08-03 21:56:25 +0930

    create referee repository and service classes

---
+++ app/Repositories/RefereeRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\User;
+use App\Models\UserReferee;
+use Illuminate\Database\Eloquent\MassAssignmentException;
+use Illuminate\Database\Eloquent\InvalidCastException;
+
+class RefereeRepository
+{
+
+    /**
+     * @param User $user 
+     * @param array $data 
+     * @return UserReferee 
+     * @throws MassAssignmentException 
+     * @throws InvalidArgumentException 
+     * @throws InvalidCastException 
+     */
+    public function createUserReferee(User $user, array $data): UserReferee
+    {
+        $referee = new UserReferee();
+        $referee->fill($data);
+        $referee->{UserReferee::FIELD_USER_ID} = $user->{User::FIELD_ID};
+        $referee->save();
+
+        return $referee;
+    }
+
+    /**
+     * @param UserReferee $referee 
+     * @param array $data 
+     * @return bool 
+     * @throws MassAssignmentException 
+     * @throws InvalidArgumentException 
+     * @throws InvalidCastException 
+     */
+    public function updateUserReferee(UserReferee $referee, array $data): bool
+    {
+        $referee->fill($data);
+        return $referee->save();
+    }
+}
