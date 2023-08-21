3b6dea5b608ec0b8ccc5e96f59c872d0854b0530
Josh Campbell <josh.campbell4000@gmail.com>
2023-08-21 23:50:28 +0930

    update the JobController's quickApply method to use a shortcode pipeline for generating the prompt

--- app/Http/Controllers/Api/JobListings/JobListingController.php
+++ app/Http/Controllers/Api/JobListings/JobListingController.php
@@ -14,6 +14,9 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use OpenAI\Laravel\Facades\OpenAI;
+use App\Models\Prompt;
+use App\Pipelines\Prompts\PromptPayloadFactory;
+use App\Pipelines\Prompts\PromptShortcodeService;
 
 class JobListingController extends ApiController
 {
@@ -21,6 +24,7 @@ class JobListingController extends ApiController
     public function __construct(
         Request $request, 
         protected JobListingRepository $jobListingRepository,
+        protected PromptShortcodeService $promptShortcodeService
     )
     {
         parent::__construct($request);
@@ -73,19 +77,23 @@ class JobListingController extends ApiController
      */
     public function quickApply(Request $request): JsonResponse
     {
-        $data = $request->get('prompt');
+        $content = Prompt::STANDARD_TEST;
+
+        $payload = PromptPayloadFactory::create(['user' => Auth::user()]);
+
+        $filledPrompt = $this->promptShortcodeService->handle($content, $payload);
 
         $chat = OpenAI::chat()->create([
             'model' => 'gpt-3.5-turbo',
             'messages' => [
-                ['role' => 'user', 'content' => $data],
+                ['role' => 'user', 'content' => $filledPrompt],
             ],
         ]);
 
         try {
             return $this->formatResponse([
+                'prompt' => $filledPrompt,
                 'response' => $chat['choices'][0]['message']['content'],
-                'models' => OpenAI::models()->list()
             ]);
         } catch (Exception $exception) {
             return $this->formatResponse([
