7f6826c2bab8747feedddfa51a4c45ca280138b0
Josh Campbell <josh.campbell4000@gmail.com>
2023-08-24 21:43:24 +0930

    create ai response and prompt migrations, controllers, models, requests, seeders

---
+++ database/migrations/2023_08_22_131459_create_ai_prompt_and_responses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prompts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::create('ai_responses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prompts');
+        Schema::dropIfExists('ai_responses');
+    }
+};
