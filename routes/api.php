50e39372c7dba1cf6ea812623e148b46a8e3255a
Josh Campbell <josh.campbell4000@gmail.com>
2023-09-04 21:21:00 +0930

    add route to get job applications

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Users\UserController;
-use App\Http\Controllers\Api\JobListings\JobListingController;
+use App\Http\Controllers\Api\Jobs\JobsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -79,9 +79,10 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
         }); 
     });
 
-    Route::prefix('jobs')->controller(JobListingController::class)->group(function () {
+    Route::prefix('jobs')->controller(JobsController::class)->group(function () {
         Route::get('/', 'getJobs');
         Route::get('/recommended-jobs', 'getRecommendedJobs');
+        Route::get('/applications', 'getApplications');
         Route::get('/{jobListing}/quick-apply', 'quickApply');
     });
 
