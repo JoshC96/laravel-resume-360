238152f1e08e3c571fa10ad62ecdcefefe064b1d
Josh Campbell <josh.campbell4000@gmail.com>
2023-09-10 00:32:43 +0930

    update cover letter prompts

--- app/Models/Prompt.php
+++ app/Models/Prompt.php
@@ -24,8 +24,8 @@ class Prompt extends Model
         self::FIELD_ID
     ];
 
-    public const PREMIUM_MAKE_COVER_LETTER = 'Use the following JSON data to create a cover letter for a job role as {{role}}. The "name" key of the JSON is the applicants name, use this in the letter. The letter should be addressed to {{entity_contact}} {{user-profile}}';
-    public const STANDARD_MAKE_COVER_LETTER = 'Use the following JSON data to create a cover letter for a job role as {{role}}. The letter should be addressed to {{entity-contact}} {{user-profile}}';
+    public const PREMIUM_MAKE_COVER_LETTER = 'Use the following JSON data to create a cover letter for a job role as {{role}}. Exclude headers such as the date, name, and addresses. Ensure there are no details included in the cover letter that are false or not included on the profiled. The "name" key of the JSON is the applicants name, use this in the letter. The letter should be addressed to {{entity_contact}} {{user-profile}}';
+    public const STANDARD_MAKE_COVER_LETTER = 'Use the following JSON data to create a cover letter for a job role as {{role}}. Exclude headers such as the date, name, and addresses.  The letter should be addressed to {{entity-contact}} {{user-profile}}';
     public const PREMIUM_MAKE_RESUME = 'Use the following JSON data to create a resume for a job role as {{role}}. {{user-profile}}';
     public const STANDARD_MAKE_RESUME = 'Use the following JSON data to create a resume for a job role as {{role}}. {{user-profile}}';
 
