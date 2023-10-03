988d7d3a725abe7240978737672228b73ae20e73
Josh Campbell <josh.campbell4000@gmail.com>
2023-10-03 18:59:13 +1030

    prevent job listing repository only ever using the current logged in user

--- app/Repositories/JobListingRepository.php
+++ app/Repositories/JobListingRepository.php
@@ -12,18 +12,16 @@ class JobListingRepository
 {
 
     /**
-     * @param User $user 
      * @param array $data 
      * @return JobListing 
      * @throws MassAssignmentException 
      * @throws InvalidArgumentException 
      * @throws InvalidCastException 
      */
-    public function createJobListing(User $user, array $data): JobListing
+    public function createJobListing(array $data): JobListing
     {
         $job = new JobListing();
         $job->fill($data);
-        $job->{JobListing::FIELD_ENTITY_ID} = $user->{User::RELATION_ENTITY_CONTACT}->{EntityContact::FIELD_ENTITY_ID};
         $job->save();
 
         return $job;
