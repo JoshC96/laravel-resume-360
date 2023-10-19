d655fcd23ecaf7e1a39b7cef739c0017782ec742
Josh Campbell <josh.campbell4000@gmail.com>
2023-10-20 01:46:17 +1030

    add provider entity type

--- app/Enums/EntityType.php
+++ app/Enums/EntityType.php
@@ -5,13 +5,14 @@ namespace App\Enums;
 enum EntityType: int
 {
     case NONE = 0;
-    case RECRUITER = 1;
-    case SMALL_BUSINESS = 2;
-    case CORPORATION = 3;
-    case PARTNERSHIP = 4; 
-    case NON_PROFIT = 5; 
-    case GOVERNMENT_ENTITY = 6;
-    case SOLE_TRADER = 7;
+    case PROVIDER = 1;
+    case RECRUITER = 2;
+    case SMALL_BUSINESS = 3;
+    case CORPORATION = 4;
+    case PARTNERSHIP = 5; 
+    case NON_PROFIT = 6; 
+    case GOVERNMENT_ENTITY = 7;
+    case SOLE_TRADER = 8;
     
     /**
      * @param int $value 
@@ -22,6 +23,7 @@ enum EntityType: int
         return match ($value) {
             self::NONE->value => 'None',
             self::RECRUITER->value => 'Recruiter',
+            self::PROVIDER->value => 'Provider',
             self::SMALL_BUSINESS->value => 'Small Business',
             self::CORPORATION->value => 'Corporation',
             self::PARTNERSHIP->value => 'Partnership',
