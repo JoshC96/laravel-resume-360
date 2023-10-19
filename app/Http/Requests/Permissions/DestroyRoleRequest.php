dc1eb93b46bd6c67ef26df786a2d4d0ad18846b7
Josh Campbell <josh.campbell4000@gmail.com>
2023-10-20 01:46:02 +1030

    add permission and role request and resource classes

---
+++ app/Http/Requests/Permissions/DestroyRoleRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Referee;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class DestroyRoleRequest extends FormRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        /** @var User $user */
+        $user = Auth::user();
+        return $user->can('edit-permissions');
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+        ];
+    }
+}
---
+++ app/Http/Requests/Permissions/StoreRoleRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Permissions;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreRoleRequest extends FormRequest
+{
+
+    public const REQUEST_NAME = 'name';
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        /** @var User $user */
+        $user = Auth::user();
+        return true;
+        // return $user->can('edit-permissions');
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            self::REQUEST_NAME => ['string', 'required'],
+        ];
+    }
+}
